121bae9e26b19a80ccc4a1cd6f60f9c348ec63a6
Askhat Telzhanov <askhat@codebusters.team>
2019-05-13 04:29:57 +0600

    back done

--- api/urls.py
+++ api/urls.py
@@ -1,2 +1,16 @@
+from django.urls import path
 
-urlpatterns = []
+from api.views import CategoryList, ProductList, CategoryDetail, BasketList
+
+from api import views
+
+urlpatterns = [
+    path('categories', CategoryList.as_view()),
+    path('products', ProductList.as_view()),
+    path('category/<int:pk>', CategoryDetail.as_view()),
+    path('basket', BasketList.as_view()),
+    path('login', views.login),
+    path('logout', views.logout),
+    path('history', views.get_order_history),
+    path('order', views.make_order)
+]
